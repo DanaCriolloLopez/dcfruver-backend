53b7d3cd09f7817fc2f20a45e5f6b1cd55d3dd5d
Dana Criollo <dana.criollo.ljfj@gmail.com>
2023-07-29 12:23:15 -0500

    Cambio test sinconización BD a test conexión BD

--- server.js
+++ server.js
@@ -3,12 +3,10 @@ import { sequelize } from "./Database/database.js";
 //Test a Base de datos
 const testsDB = async () => {
     try {
-        // Sincroniza los modelos con la base de datos
-        await sequelize.sync();
-    
-        console.log('Modelos sincronizados correctamente con la base de datos.');
+        await sequelize.authenticate();
+        console.log("Conexión realizada con éxito");
     } catch (error) {
-        console.error('Error al sincronizar modelos con la base de datos:', error);
+        console.log(`Error al realizar conexión ${error}`);
     }
     
 }
